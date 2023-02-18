a7fe9b263bda4175cc20ac88bc36470676bcb044
Sara Talebzadeh <sara9170@gmail.com>
2023-02-18 13:23:09 +0330

    #001 hello world

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="hello">
+    HELLO WORLD!
+  </div>
+</template>
+<style scoped>
+  .hello{
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 2 rem;
+    padding: 0 auto;
+  }
+</style>
